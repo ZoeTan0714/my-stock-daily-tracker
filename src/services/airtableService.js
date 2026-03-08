a5fbcfbfbc3d2b2b416decc5aa6420444fa3a804
Zoe Tan <zoe.txy@outlook.com>
2026-03-09 00:09:30 +0800

    commit

--- src/services/airtableService.js
+++ src/services/airtableService.js
@@ -1,5 +1,5 @@
 
-const airtble_API_key = 'patCv9CLnTogsKBUh.1aec7509b86f819412bc5d2e7f72ad236a70e995edc429ef7758cf21b17a9c3b'
+// const airtble_API_key = 'patCv9CLnTogsKBUh.1aec7509b86f819412bc5d2e7f72ad236a70e995edc429ef7758cf21b17a9c3b'
 
 const airtableService = {
     getWatclist: async () => {
@@ -9,7 +9,7 @@ const airtableService = {
 
 
 
-                
+
             )
         }
     }
